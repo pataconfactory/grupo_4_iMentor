ab09a2e97d8edb45dd2303833f2e3d0b4ce49f11
QuizairaRodriguez <kiss02andre@gmail.com>
2022-03-22 22:20:20 -0300

    CRUD y Vistas faltantes

--- Proyecto/src/controllers/productsController.js
+++ Proyecto/src/controllers/productsController.js
@@ -1,17 +1,109 @@
 const path = require('path');
+const fs = require('fs');
+
+let productsJSON = fs.readFileSync(path.join(__dirname, '../data/products.json'), 'utf-8');
+let products = JSON.parse (productsJSON);
 
 const productsController = {
 
-    productCart: function(req, res) {
-        res.render(path.join(__dirname, '../views/products/productCart'))
-    },
-
     products: function(req, res) {
-       res.render(path.join(__dirname, '../views/products/products'))
+       res.render(path.join(__dirname, '../views/products/products'), {products})
     },
 
     create: function(req, res) {
-        res.render(path.join(__dirname, '../views/products/productEdition'))
+        res.render(path.join(__dirname, '../views/products/productCreate'))
+    },
+
+    store: function(req, res) {
+        let ultimoProducto = products.pop();
+        let idUltimoProducto = ultimoProducto.id;
+        let newProduct = req.body;
+        newProduct.mentor_avatar = null;
+        let filename = req.file.filename;
+        newProduct.image = filename;
+        newProduct.id = idUltimoProducto + 1;
+        products.push(ultimoProducto);
+        products.push(newProduct);
+        let newProductsJSON = JSON.stringify(products);
+        console.log(newProductsJSON); /*Se ve en la Terminal*/
+        fs.writeFileSync(path.join(__dirname, '../data/products.json'), newProductsJSON);
+		res.redirect('/products/');
+    },
+
+    detail: function(req, res) {
+        let idProducto = req.params.id;
+        let productoRequerido = {};
+        for(let i=0; i<products.length; i++){
+            if(products[i].id == idProducto){
+                let productoRequerido = products[i];  
+                res.render(path.join(__dirname, '../views/products/detail'), {productoRequerido})
+            }
+        }  
+    },
+
+    edit: function(req, res) {
+        let idProducto = req.params.id;
+        let productoEditar = {};
+        for(let i=0; i<products.length; i++){
+            if(products[i].id == idProducto){
+                let productoEditar = products[i];  
+                res.render(path.join(__dirname, '../views/products/productEdition'), {productoEditar})
+            }
+        }  
+    },
+
+    update: function(req, res) {
+        let idProducto = req.params.id;
+        let productEnviado = req.body;
+        let file = req.file;
+		products.forEach(function (elemento){
+			if (elemento.id == idProducto) {
+				elemento.name = productEnviado.name;
+				elemento.description = productEnviado.description;
+				elemento.service = productEnviado.service;
+                elemento.category = productEnviado.category;
+				elemento.mentor = productEnviado.mentor;
+                elemento.mentor_country = productEnviado.country;
+                elemento.mentor_titulo = productEnviado.titulo;
+                elemento.duration = productEnviado.duration;
+                elemento.horario = productEnviado.horario;
+                elemento.price = productEnviado.price;
+                elemento.discount = productEnviado.discount;
+                if(file !== undefined){
+                    const filename = req.file.filename;
+                    elemento.image = filename;
+                }
+			}
+		});
+        let productsJSON = JSON.stringify(products);
+        console.log(productsJSON);
+        fs.writeFileSync(path.join(__dirname, '../data/products.json'), productsJSON);
+        res.redirect('/products/');
+    },
+
+    destroy: function(req, res) {
+        let idProducto = req.params.id;
+        let productoEliminar = {};
+        for(let i=0; i<products.length; i++){
+            if(products[i].id == idProducto){
+                productoEliminar = products[i];   
+            }
+        };
+        products = products.filter(function (elemento){
+			return (elemento != productoEliminar);
+        });
+        let productsJSON = JSON.stringify(products);
+        console.log(productsJSON);
+        fs.writeFileSync(path.join(__dirname, '../data/products.json'), productsJSON);
+        res.redirect('/products/');
+    },
+
+    productServices: function(req, res) {
+        res.render(path.join(__dirname, '../views/products/productServices'))
+    },
+
+    productCart: function(req, res) {
+        res.render(path.join(__dirname, '../views/products/productCart'))
     },
 
     /*search:*/
--- Proyecto/src/routes/products.js
+++ Proyecto/src/routes/products.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 
 const productsController = require('../controllers/productsController');
 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/img/products')
+    },
+    filename: function (req, file, cb) {
+        cb(null, 'product'+'-'+ Date.now() + path.extname(file.originalname))
+    }
+});
+    
+var upload = multer({ storage: storage });
+
+//Rutas
 
 router.get('/', productsController.products);
 
-router.get('/productEdition', productsController.create);
+router.get('/create', productsController.create);
+router.post('/', upload.single('image'), productsController.store);
 
-router.get('/:id/', productsController.detail); 
+router.get('/detail/:id', productsController.detail)
+
+router.get('/edit/:id', productsController.edit)
+router.put('/edit/:id', upload.single('image'), productsController.update)
+
+router.delete('/delete/:id', productsController.destroy)
 
 router.get('/productCart', productsController.productCart);
 
+router.get('/productServices', productsController.productServices);
 
 /*router.get('/search', productsController.search); */
 
