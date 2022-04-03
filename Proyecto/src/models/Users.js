a3e852ca5b8254e0b916d9e11d814bff342e37d2
VictoriaDefagot <defagotvictoria@gmail.com>
2022-04-03 10:11:23 -0300

    Model Users.js created

---
+++ Proyecto/src/models/Users.js
@@ -0,0 +1,58 @@
+const { all } = require('express/lib/application');
+const fs = require('fs');
+const path = require('path');
+
+const Users = {
+
+fileName = path.join(__dirname, '../data/users.json'),
+
+getData: function() {
+    return JSON.parse(fs.readFileSync(this.fileName), 'utf-8');
+},
+
+findAll: function(){
+    return this.getData();
+},
+
+findByPk: function(id) {
+    let allUsers = this.findAll();
+    let userFound = allUsers.find(oneUser => oneUser.id === id);
+    return userFound;
+},
+
+generateID: function() {
+    let allUsers = this.findAll();
+    let lastUser = allUsers.pop()
+
+    if(lastUser) {
+        return lastUser.id + 1;
+    }
+
+    return 1;
+},
+
+createUser: function(userData) {
+    let allUsers = this.findAll();
+    let newUser = {
+        id: this.generateID(),
+        ...userData
+    }
+    allUsers.push(newUser);
+    fs.writeFileSync(this.fileName, JSON.stringify(allUsers), null, ' ');
+},
+
+deleteUser: function(id) {
+    let allUsers = this.findAll();
+    let finalUsers = allUsers.filter(oneUser => oneUser.id !== id);
+    fs.writeFileSync(this.fileName, JSON.stringify(finalUsers), null, ' ');
+},
+
+editUser: function(id) {
+    let allUsers = this.findAll();
+
+    
+}
+
+}
+
+module.exports = Users;
