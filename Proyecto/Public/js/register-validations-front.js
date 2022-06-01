9c197e0cdbd5a2ff4fc02f005cd67b9ed65720f5
VictoriaDefagot <defagotvictoria@gmail.com>
2022-06-01 12:13:06 -0300

    Front-end validations -submit event- in Register and Login pages

--- Proyecto/Public/js/register-validations-front.js
+++ Proyecto/Public/js/register-validations-front.js
@@ -1,4 +1,115 @@
 window.addEventListener('load', function () {
+
+    let form = this.document.querySelector('.register-form');
+    form.addEventListener('submit', function (e) {
+
+        let errors = [];
+
+        let password = document.querySelector('#password');
+        let passwordLength = document.querySelector('.password-length');
+        let passwordValue = password.value;
+        if (!passwordValue) {
+            passwordLength.innerHTML = 'Debes completar la contraseña';
+            errors.push('Debes completar la contraseña');
+        } else if (passwordValue.length < 8) {
+            passwordLength.innerHTML = 'La constraseña debe tener al menos 8 caracteres';
+            errors.push('La constraseña debe tener al menos 8 caracteres');
+        }
+
+        let email = document.querySelector('#email');
+        let emailError = document.querySelector('.email-error');
+        let emailValue = email.value;
+        if (!emailValue) {
+            emailError.innerHTML = 'Debes completar el correo electrónico';
+            errors.push('Debes completar el correo electrónico');
+        }
+        let input = email;
+        emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if (!emailRegex.test(input.value)) {
+            emailError.innerHTML = 'La dirección de correo electrónico no es válida';
+            errors.push('La dirección de correo electrónico no es válida');
+        }
+
+        let firstName = document.querySelector('#first_name');
+        let firstNameError = document.querySelector('.first_name-error');
+        let firstNameValue = firstName.value;
+        if (!firstNameValue) {
+            firstNameError.innerHTML = 'Debes completar el nombre';
+            errors.push('Debes completar el nombre');
+        } else if (firstNameValue.length < 2) {
+            firstNameError.innerHTML = 'El nombre debe tener al menos 2 caracteres';
+            errors.push('El nombre debe tener al menos 2 caracteres');
+        }
+
+        let lastName = document.querySelector('#last_name');
+        let lastNameError = document.querySelector('.last_name-error');
+        let lastNameValue = lastName.value;
+        if (!lastNameValue) {
+            lastNameError.innerHTML = 'Debes completar el apellido';
+            errors.push('Debes completar el apellido')
+        } else if (lastNameValue.length < 2) {
+            lastNameError.innerHTML = 'El apellido debe tener al menos 2 caracteres';
+            errors.push('El apellido debe tener al menos 2 caracteres');
+        }
+
+        let userName = document.querySelector('#user_name');
+        let userNameError = document.querySelector('.user_name-error');
+        let userNameValue = userName.value;
+        if (!userNameValue) {
+            userNameError.innerHTML = 'Debes completar el nombre de usuario';
+            errors.push('Debes completar el nombre de usuario');
+        }
+
+        let dateBirth = document.querySelector('#date_birth');
+        let dateBirthError = document.querySelector('.date_birth-error');
+        let dateBirthValue = dateBirth.value;
+        if (!dateBirthValue) {
+            dateBirthError.innerHTML = 'Debes completar la fecha de nacimiento';
+            errors.push('Debes completar la fecha de nacimiento');
+        }
+
+        let genre = document.querySelector('#genero');
+        let genreError = document.querySelector('.genre-error');
+        let genreValue = genre.value;
+        if (!genreValue) {
+            genreError.innerHTML = 'Debes completar el género';
+            errors.push('Debes completar el género');
+        }
+
+        let country = document.querySelector('#country');
+        let countryError = document.querySelector('.country-error');
+        let countryValue = country.value;
+        if (!countryValue) {
+            countryError.innerHTML = 'Debes completar el país';
+            errors.push('Debes completar el país');
+        }
+
+        let title = document.querySelector('#title');
+        let titleError = document.querySelector('.title-error');
+        let titleValue = title.value;
+        if (!titleValue) {
+            titleError.innerHTML = 'Debes completar el título';
+            errors.push('Debes completar el título');
+        }
+
+        let avatar = document.querySelector('#avatar');
+        let avatarError = document.querySelector('.avatar-error');
+        let avatarValue = avatar.value;
+        if (!avatarValue) {
+            avatarError.innerHTML = 'Debes completar la imagen de usuario';
+            errors.push('Debes completar la imagen de usuario');
+        } else {
+            if (!(avatarValue.includes('.jpg') || avatarValue.includes('.jpeg') || avatarValue.includes('.png') || avatarValue.includes('.gif'))) {
+                avatarError.innerHTML = 'El formato de archivo no es válido, las extensiones permitidas son .jpg, .jpeg, .png y .gif';
+                errors.push('El formato de archivo no es válido, las extensiones permitidas son .jpg, .jpeg, .png y .gif');
+            }
+        }
+
+        if (errors.length > 0) {
+            e.preventDefault();
+        }
+    });
+
     let password = document.querySelector('#password');
     let passwordLength = document.querySelector('.password-length');
 
@@ -75,6 +186,8 @@ window.addEventListener('load', function () {
         let userNameValue = userName.value;
         if (!userNameValue) {
             userNameError.innerHTML = 'Debes completar el nombre de usuario';
+        } else {
+            userNameError.innerHTML = '';
         }
     });
 
@@ -84,6 +197,8 @@ window.addEventListener('load', function () {
         let dateBirthValue = dateBirth.value;
         if (!dateBirthValue) {
             dateBirthError.innerHTML = 'Debes completar la fecha de nacimiento';
+        } else {
+            dateBirthError.innerHTML = '';
         }
     });
 
@@ -93,6 +208,8 @@ window.addEventListener('load', function () {
         let genreValue = genre.value;
         if (!genreValue) {
             genreError.innerHTML = 'Debes completar el género';
+        } else {
+            genreError.innerHTML = '';
         }
     });
 
@@ -102,6 +219,8 @@ window.addEventListener('load', function () {
         let countryValue = country.value;
         if (!countryValue) {
             countryError.innerHTML = 'Debes completar el país';
+        } else {
+            countryError.innerHTML = '';
         }
     });
 
@@ -111,6 +230,8 @@ window.addEventListener('load', function () {
         let titleValue = title.value;
         if (!titleValue) {
             titleError.innerHTML = 'Debes completar el título';
+        } else {
+            titleError.innerHTML = '';
         }
     });
 
@@ -121,8 +242,10 @@ window.addEventListener('load', function () {
         if (avatarValue) {
             if (!(avatarValue.includes('.jpg') || avatarValue.includes('.jpeg') || avatarValue.includes('.png') || avatarValue.includes('.gif'))) {
                 avatarError.innerHTML = 'El formato de archivo no es válido, las extensiones permitidas son .jpg, .jpeg, .png y .gif';
+            } else {
+                avatarError.innerHTML = '';
             }
         }
     });
 
-})
+});
--- Proyecto/Public/js/register-validations-mentors-front.js
+++ Proyecto/Public/js/register-validations-mentors-front.js
@@ -1,11 +1,55 @@
 window.addEventListener('load', function () {
-   
+
+    let form = this.document.querySelector('.register-form');
+    form.addEventListener('submit', function (e) {
+
+        let errors = [];
+
+        let description = document.querySelector('#description');
+        let descriptionError = document.querySelector('.description-error');
+        let descriptionValue = description.value;
+        if (!descriptionValue) {
+            descriptionError.innerHTML = 'Debes completar una descripción sobre ti';
+            errors.push('Debes completar una descripción sobre ti');
+        }
+
+        let hourPrice = document.querySelector('#hour_price');
+        let hourPriceError = document.querySelector('.hour_price-error');
+        let hourPriceValue = hourPrice.value;
+        if (!hourPriceValue) {
+            hourPriceError.innerHTML = 'Debes completar el precio por hora';
+            errors.push('Debes completar el precio por hora');
+        }
+
+        let bank = document.querySelector('#bank');
+        let bankError = document.querySelector('.bank-error');
+        let bankValue = bank.value;
+        if (!bankValue) {
+            bankError.innerHTML = 'Debes completar el banco';
+            errors.push('Debes completar el banco');
+        }
+
+        let cbu = document.querySelector('#cbu');
+        let cbuError = document.querySelector('.cbu-error');
+        let cbuValue = cbu.value;
+        if (!cbuValue) {
+            cbuError.innerHTML = 'Debes completar el CBU';
+            errors.push('Debes completar el CBU');
+        }
+
+        if (errors.length > 0) {
+            e.preventDefault();
+        }
+    });
+
     let description = document.querySelector('#description');
     let descriptionError = document.querySelector('.description-error');
     description.addEventListener('blur', function () {
         let descriptionValue = description.value;
         if (!descriptionValue) {
             descriptionError.innerHTML = 'Debes completar una descripción sobre ti';
+        } else {
+            descriptionError.innerHTML = '';
         }
     });
 
@@ -15,6 +59,8 @@ window.addEventListener('load', function () {
         let hourPriceValue = hourPrice.value;
         if (!hourPriceValue) {
             hourPriceError.innerHTML = 'Debes completar el precio por hora';
+        } else {
+            hourPriceError.innerHTML = '';
         }
     });
 
@@ -24,6 +70,8 @@ window.addEventListener('load', function () {
         let bankValue = bank.value;
         if (!bankValue) {
             bankError.innerHTML = 'Debes completar el banco';
+        } else {
+            bankError.innerHTML = '';
         }
     });
 
@@ -33,7 +81,8 @@ window.addEventListener('load', function () {
         let cbuValue = cbu.value;
         if (!cbuValue) {
             cbuError.innerHTML = 'Debes completar el CBU';
+        } else {
+            cbuError.innerHTML = '';
         }
     });
-
-})
+});
