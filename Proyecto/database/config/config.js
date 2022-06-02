958e5777668e5b730d244c5a5412c3b8977de06d
QuizairaRodriguez <kiss02andre@gmail.com>
2022-06-01 23:20:13 -0300

    Detalles en las vistas

--- Proyecto/database/config/config.js
+++ Proyecto/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
-    "username": "",
-    "password": '',
+    "username": "root",
+    "password": null,
     "database": "imentor_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
